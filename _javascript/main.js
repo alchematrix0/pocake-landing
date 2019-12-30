daa3f6b5c084c475988fade00f89df17acf8893b
alchematrix0 <alchematrix0@gmail.com>
2019-12-29 20:57:16 -0800

    remove dom content loaded, we are just declaring a function

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,11 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOMContentLoaded')
-  function scrollPageTo(targetId) {
-    let target = document.getElementById(targetId)
-    target.scrollIntoView({behavior: "smooth"})
-    if (targetId === 'video') {
-      let video = document.getElementById('videoPlayer')
-      video.play()
-    }
+function scrollPageTo(targetId) {
+  let target = document.getElementById(targetId);
+  target.scrollIntoView({ behavior: 'smooth' });
+  if (targetId === 'video') {
+    let video = document.getElementById('videoPlayer');
+    video.play();
   }
-})
+}
